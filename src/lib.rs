a81087452618ef51d37479d7e086e73ca87ba9ff
ramosdetrigo <julia.and.ramos@gmail.com>
2026-05-06 19:23:52 -0300

    deu tudo certo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-// #![allow(dead_code)]
-
 pub mod persistent_btree;
 
 #[cfg(test)]
@@ -9,20 +7,28 @@ mod pbtree_test {
     fn basic() {
         let mut tree: PBTree<i32> = PBTree::new();
         tree.push(1);
-        tree.push(5);
-        tree.push(-5);
-        tree.push(10);
-        tree.push(13);
+        tree.push(3);//1
         tree.push(4);
-        tree.push(-3);
-        tree.push(-7);
-        tree.push(-5);
+        tree.push(5);
+        tree.push(6);
+        tree.remove(&3);//2
+        tree.remove(&4);//3
+        tree.remove(&5);//4
+        tree.remove(&6);//5
+        tree.push(2);//6
+        tree.push(3);//1
+        tree.remove(&3);//2
+        tree.push(3);//3
+        tree.remove(&3);//4
+        tree.push(3);//5
+        tree.remove(&3);//6
+        tree.push(3);//7
 
-        for i in 0..=tree.version() {
+
+
+        for i in 1..=tree.version() {
             println!("====== TREE AT VERSION {i} ======");
-            println!("{:?}", tree.successor(&10, i));
-            // println!("{}", tree.list(i));
+            println!("{}", tree.list_tree(i));
         }
-
     }
 }
--- src/persistent_btree/node.rs
+++ src/persistent_btree/node.rs
@@ -1,48 +1,41 @@
+use std::fmt::Debug;
+
 use super::*;
 
 pub type NodePtr<T> = NonNull<Node<T>>;
 pub type Link<T> = Option<NodePtr<T>>;
 
 const P: usize = 3; // Número de ponteiros em um nó
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Node<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
     pub value: T,
     pub parent: Link<T>,
     pub left: Link<T>,
     pub right: Link<T>,
+    pub tree_ptr: NonNull<PBTree<T>>,
     // NOTE: Since we're using a vec, the P and 2P restrictions need to be enforced via code!
     return_pts: Vec<ReturnPtr<T>>, // P = 3
     mods: Vec<Modification<T>>,    // 2P = 6
 }
 
-#[allow(dead_code)]
-pub struct NodeSnapshot<T>
-where
-    T: PartialOrd + Clone,
-{
-    pub value: T,
-    pub parent: Link<T>,
-    pub left: Link<T>,
-    pub right: Link<T>,
-}
-
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Modification<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
     version: usize,
     modification: ModTarget<T>,
 }
 
-#[allow(dead_code)]
 impl<T> Modification<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
+    #[inline]
+    #[must_use]
     pub fn parent(link: Link<T>, version: usize) -> Self {
         Self {
             version,
@@ -50,6 +43,8 @@ where
         }
     }
 
+    #[inline]
+    #[must_use]
     pub fn left(link: Link<T>, version: usize) -> Self {
         Self {
             version,
@@ -57,6 +52,8 @@ where
         }
     }
 
+    #[inline]
+    #[must_use]
     pub fn right(link: Link<T>, version: usize) -> Self {
         Self {
             version,
@@ -64,6 +61,9 @@ where
         }
     }
 
+    #[inline]
+    #[must_use]
+    #[allow(dead_code)]
     pub fn value(value: T, version: usize) -> Self {
         Self {
             version,
@@ -73,24 +73,25 @@ where
 }
 
 /// Represents a modification to a specific field of the node
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum ModTarget<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
     Parent(Link<T>),
     Left(Link<T>),
     Right(Link<T>),
+    #[allow(dead_code)]
     Value(T),
 }
 
 /// A return pointer to another node. \
 /// This reads as "this node is Left/Right/Parent of another node": \
 /// `ReturnPtr::LeftOf(Y)` inside `X.return_pts` would mean `Y.left = X`
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum ReturnPtr<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
     ParentOf(NodePtr<T>),
     LeftOf(NodePtr<T>),
@@ -99,7 +100,7 @@ where
 
 impl<T> ReturnPtr<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
     /// Adds a modifier to the return_ptr's node to point to the new node
     pub fn redirect_node(&mut self, version: usize, new_target: Link<T>) {
@@ -110,22 +111,48 @@ where
         };
 
         unsafe {
-            return_ptr.as_mut().add_mod(Modification {
+            // We don't need to update return_ptr since add_mod_full should already do that
+            // if a new node is created.
+            let _ = return_ptr.as_mut().add_mod(Modification {
                 version,
                 modification,
             });
+        };
+    }
+
+    #[inline]
+    #[must_use]
+    /// "Unwraps" the ReturnPtr
+    pub fn ptr(&self) -> NodePtr<T> {
+        match self {
+            ReturnPtr::LeftOf(ptr) => *ptr,
+            ReturnPtr::RightOf(ptr) => *ptr,
+            ReturnPtr::ParentOf(ptr) => *ptr,
+        }
+    }
+
+    #[inline]
+    /// Sets the ReturnPtr pointer into a new one
+    pub fn set_ptr(&mut self, new_ptr: NodePtr<T>) {
+        match self {
+            ReturnPtr::LeftOf(ptr) => *ptr = new_ptr,
+            ReturnPtr::RightOf(ptr) => *ptr = new_ptr,
+            ReturnPtr::ParentOf(ptr) => *ptr = new_ptr,
         };
     }
 }
 
 impl<T> Node<T>
 where
-    T: PartialOrd + Clone,
+    T: PartialOrd + Clone + Debug,
 {
     // Node constructor
-    pub fn new(value: T) -> Self {
+    #[must_use]
+    #[inline]
+    pub fn new(value: T, tree_ptr: NonNull<PBTree<T>>) -> Self {
         Self {
             value,
+            tree_ptr,
             parent: None,
             left: None,
             right: None,
@@ -138,6 +165,9 @@ where
     /// where the mods field is full. \
     /// Returns `Some(NodePtr<T>)` if the mod limit has been reached
     /// and a new node has been created. `None` otherwise.
+    /// Marked as must_use to help prevent stale node bugs.
+    /// Use as `let _ = [...]` if new links don't matter.
+    #[must_use]
     pub fn add_mod(&mut self, modifier: Modification<T>) -> Link<T> {
         // Constraint: the mods field max size is P * 2
         if self.mods.len() == P * 2 {
@@ -149,7 +179,7 @@ where
             self.mods.push(modifier);
         // Case 2: pointer update
         } else {
-            let current = self.with_mods();
+            let current = self.modded_clone();
 
             // old_ptr: Ponteiro pro nó antigo do campo modificado
             // ret_ptr: Padrão do ponteiro de retorno esperado
@@ -167,7 +197,7 @@ where
             // Remove o nó atual dos ponteiros de retorno do nó antigo, se necessário
             if let Some(mut node) = old_ptr {
                 let node = unsafe { node.as_mut() };
-                node.return_pts.retain(|ptr| *ptr != ret_ptr);
+                node.remove_return_ptr(ret_ptr.clone());
             }
 
             // Adiciona o nó atual nos ponteiros de retorno do novo nó
@@ -183,41 +213,57 @@ where
     /// Handles the case for add_mod() where the mods field is full.
     #[must_use]
     fn add_mod_full(&mut self, modifier: Modification<T>) -> NodePtr<T> {
+        println!("mod_full");
         // Create node copy with empty mods field
-        let mut new_node = self.with_mods();
-
-        // Move return pointers from the old node to the new one, since things
-        // will now be pointing to the new node instead of the old one.
-        new_node.return_pts = std::mem::take(&mut self.return_pts);
-
-        // Save a copy of the return pointers for updating the nodes
-        let return_pts = new_node.return_pts.clone();
+        let new_node = self.modded_clone();
 
         // Then convert new_node into a heap-allocated link
         let new_node = new_node.into_link();
+        let new_node_ptr = new_node.unwrap();
 
         // And update nodes that pointed to the old node to point to the new one
-        for mut return_ptr in return_pts {
+        // This will also add the return pointers to the new node.
+        let self_ptr = self.as_ptr();
+        for return_ptr in &mut self.return_pts {
+            // Redirects the target's return pointers to the new node
+            let target = unsafe { return_ptr.ptr().as_mut() };
+            target.return_pts.iter_mut().for_each(|ret_ptr| {
+                if ret_ptr.ptr() == self_ptr {
+                    ret_ptr.set_ptr(new_node_ptr)
+                }
+            });
+
+            // Redirect the non-return pointers to the new node
+            // (also adds the return_ptr to the new node since that's how add_mod works.)
             return_ptr.redirect_node(modifier.version, new_node);
         }
 
         // Finally, adds the modifier to the new node.
         let node_ref = unsafe { new_node.unwrap().as_mut() };
-        node_ref.add_mod(modifier);
+        let _ = node_ref.add_mod(modifier);
+
         // Remove the mod from the list and immediately apply it:
         // The node when accessed at the current version should already have the first mod applied
         let modifier = node_ref.mods.pop().unwrap();
+        let version = modifier.version;
         node_ref.apply_mod(&modifier);
+
+        // Updates the tree's root if it changed
+        if self.parent.is_none() {
+            let tree = unsafe { self.tree_ptr.as_mut() };
+            tree.root_history.push((new_node, version));
+        }
 
         // Returns the pointer to the new node
         new_node.unwrap()
     }
 
+    #[must_use]
     pub fn latest_successor(&self) -> Link<T> {
         let mut successor: Link<T> = self.right;
         let mut curr_node = self.right;
         while let Some(node) = curr_node {
-            let latest = unsafe { node.as_ref().with_mods() };
+            let latest = unsafe { node.as_ref().modded_clone() };
             // If the value of the node is greater than the element,
             // update successor & search to the left
             if latest.value > self.value {
@@ -231,6 +277,7 @@ where
         successor
     }
 
+    #[inline]
     pub fn add_return_ptr(&mut self, ret_ptr: ReturnPtr<T>) {
         assert!(
             self.return_pts.len() < P,
@@ -240,12 +287,24 @@ where
         self.return_pts.push(ret_ptr);
     }
 
+    #[inline]
+    pub fn remove_return_ptr(&mut self, ret_ptr: ReturnPtr<T>) {
+        self.return_pts.retain(|ptr| *ptr != ret_ptr);
+    }
+
+    // #[inline]
+    // pub fn remove_return_ptr_generic(&mut self, node_ptr: NodePtr<T>) {
+    //     self.return_pts.retain(|ret_ptr| ret_ptr.ptr() != node_ptr);
+    // }
+
     /// Returns a clone of the node with all modifications applied to it and an empty mods vec
     /// CAUTION! nodes that point to the original node won't point to the "with_mods" clone!
-    pub fn with_mods(&self) -> Self {
+    /// The clone will also have an empty return_pts vec.
+    #[must_use]
+    pub fn modded_clone(&self) -> Self {
         // The node is initialized with empty return_pts and mods vecs.
         // (empty return_pts since nothing will actually be pointing to the clone).
-        let mut new_node = Self::new(self.value.clone());
+        let mut new_node = Self::new(self.value.clone(), self.tree_ptr);
         new_node.parent = self.parent;
         new_node.left = self.left;
         new_node.right = self.right;
@@ -257,10 +316,10 @@ where
         new_node
     }
 
-    /// Returns a snapshot (a "read-only copy") of the node
-    /// at a certain version of the tree
-    pub fn at_version(&self, version: usize) -> NodeSnapshot<T> {
-        let mut new_node = Self::new(self.value.clone());
+    /// Returns a snapshot of the node at a certain version of the tree
+    #[must_use]
+    pub fn snapshot(&self, version: usize) -> Self {
+        let mut new_node = Self::new(self.value.clone(), self.tree_ptr);
         new_node.parent = self.parent;
         new_node.left = self.left;
         new_node.right = self.right;
@@ -268,18 +327,16 @@ where
         for modifier in &self.mods {
             if modifier.version <= version {
                 new_node.apply_mod(modifier)
+            } else {
+                break;
             }
         }
 
-        NodeSnapshot {
-            value: new_node.value,
-            parent: new_node.parent,
-            left: new_node.left,
-            right: new_node.right,
-        }
+        new_node
     }
 
     /// Applies a modification to a node
+    #[inline]
     pub fn apply_mod(&mut self, modifier: &Modification<T>) {
         match modifier.modification.clone() {
             ModTarget::Parent(ptr) => self.parent = ptr,
@@ -290,6 +347,7 @@ where
     }
 
     /// Converts node into a heap-allocated link
+    #[inline]
     pub fn into_link(self) -> Link<T> {
         // NonNull::new already returns an option (because of the null check),
         // so we don't need to wrap it into a Some()
@@ -297,6 +355,7 @@ where
     }
 
     /// Returns a NonNull pointer to the node
+    #[inline]
     pub fn as_ptr(&self) -> NodePtr<T> {
         NonNull::from(self)
     }
