1041e12d24ef0c21059140c5f5415f671caa8dce
ramosdetrigo <julia.and.ramos@gmail.com>
2026-06-19 16:24:15 -0300

    função de impressão

--- src/veb/mod.rs
+++ src/veb/mod.rs
@@ -30,16 +30,27 @@ impl Veb {
 
     #[inline]
     #[must_use]
+    fn new_halved(&self) -> Self {
+        assert!(self.w > 2, "Folha (w = 2) tentou se dividir.");
+        Self {
+            w: self.w / 2,
+            min: None,
+            max: None,
+            clusters: HashMap::new(),
+            resumo: OnceCell::new(),
+        }
+    }
+
+    #[inline]
+    #[must_use]
     fn resumo(&self) -> &Veb {
-        self.resumo
-            .get_or_init(|| Box::new(Self::with_w(self.w / 2)))
+        self.resumo.get_or_init(|| Box::new(self.new_halved()))
     }
 
     #[inline]
     #[must_use]
     fn resumo_mut(&mut self) -> &mut Veb {
-        self.resumo
-            .get_or_init(|| Box::new(Self::with_w(self.w / 2)));
+        self.resumo.get_or_init(|| Box::new(self.new_halved()));
         self.resumo.get_mut().unwrap()
     }
 
@@ -56,6 +67,11 @@ impl Veb {
 
             if x > self.max.unwrap() {
                 self.max = Some(x);
+            }
+
+            // cria a cluster se ela não existe na hashmap ainda.
+            if !self.clusters.contains_key(&c) {
+                self.clusters.insert(c, self.new_halved());
             }
 
             if self.clusters[&c].min.is_none() {
@@ -93,8 +109,9 @@ impl Veb {
         cluster.remove(i);
 
         if cluster.min.is_none() {
-            // cluster c ficou vazio: remove ele do resumo
+            // cluster c ficou vazio: remove ele do resumo (e da memória)
             self.resumo_mut().remove(c);
+            self.clusters.remove(&c);
         }
         if self.resumo().min.is_none() {
             // Não existe cluster não-vazio: max = min
@@ -117,6 +134,7 @@ impl Veb {
         // Caso 2: checa se resposta está no cluster c
         let (c, i) = split_bits(x, self.w);
 
+        // Se o cluster não está na hashmap, é o mesmo que ele estar vazio.
         if let Some(cluster) = self.clusters.get(&c) {
             if cluster.max.is_some_and(|max| i < max) {
                 let succ_i = cluster.successor(i).unwrap();
@@ -141,6 +159,7 @@ impl Veb {
         // Caso 2: checa se resposta está no cluster c
         let (c, i) = split_bits(x, self.w);
 
+        // Se o cluster não está na hashmap, é o mesmo que ele estar vazio.
         if let Some(cluster) = self.clusters.get(&c) {
             if cluster.min.is_some_and(|min| i > min) {
                 let pred_i = cluster.predecessor(i).unwrap();
@@ -152,6 +171,70 @@ impl Veb {
         let prev_c = self.resumo().predecessor(c)?; // procura primeiro cluster não-vazio depois de C
         let prev_i = self.clusters[&prev_c].max.unwrap(); // unwrap garantido: cluster não vazio
         Some(merge_bits(prev_c, prev_i, self.w))
+    }
+
+    pub fn imp(&self) -> String {
+        // edge case: veb vazia
+        if self.min.is_none() {
+            return "Min: null".to_string();
+        }
+
+        let mut out = format!("Min: {}", self.min.unwrap());
+
+        // Se w == 2 (folha), não tem clusters pra mostrar
+        // (max == min, então não tem mais elementos)
+        if self.w == 2 {
+            return out;
+        }
+
+        // para cada cluster não-vazio
+        let summary_elems = self.resumo().collect();
+        for c in summary_elems {
+            let cluster = &self.clusters[&c];
+            let sub_elems = cluster.collect();
+
+            // reconstrói os valores e formata
+            let elementos: Vec<String> = sub_elems
+                .iter()
+                .map(|i| merge_bits(c, *i, self.w).to_string())
+                .collect();
+
+            out.push_str(&format!(", C[{}]: {}", c, elementos.join(", ")));
+        }
+
+        out
+    }
+
+    fn collect(&self) -> Vec<u32> {
+        let mut out: Vec<u32> = Vec::new();
+
+        // se a veb está vazia, não tem nada pra coletar
+        if self.min.is_none() {
+            return out;
+        }
+
+        // o mínimo está na raiz, então adiciona primeiro
+        out.push(self.min.unwrap());
+
+        // edge case: w == 2: min é o único elemento possível
+        if self.w == 2 {
+            // max == min. não se inseriria algo em um cluster dessa VEB.
+            return out;
+        }
+
+        // pra cada cluster não vazio, coleta seus elementos
+        let summary_elems = self.resumo().collect();
+        for c in summary_elems {
+            let cluster = &self.clusters[&c];
+            let sub_elems = cluster.collect();
+
+            // reconstrói os valores
+            for i in sub_elems {
+                out.push(merge_bits(c, i, self.w));
+            }
+        }
+
+        out
     }
 }
 
